14c91de463332a4ecc62211d184e3712fc080599
Akash-Tandale001 <ak.tandale123@gmail.com>
2023-03-05 23:44:24 +0530

    Build user service and it's basic endpoint

---
+++ User-Service/src/main/java/com/akash/userservice/Service/UserService.java
@@ -0,0 +1,41 @@
+package com.akash.userservice.Service;
+
+import com.akash.userservice.Entity.User;
+import com.akash.userservice.Repository.UserRepository;
+import com.akash.userservice.ValueObject.Department;
+import com.akash.userservice.ValueObject.ResponseTemplateVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@Slf4j
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public User getUserById(Long userId) {
+        return userRepository.findByUserId(userId);
+
+    }
+
+
+    public User saveUser(User user) {
+        log.info("Inside saveUser of Userservice");
+        return userRepository.save(user);
+    }
+
+    public ResponseTemplateVO getUserWithDepartment(Long userId) {
+        log.info("inside the getUserWithDepartment method of userservice");
+        ResponseTemplateVO vo = new ResponseTemplateVO();
+        User user = userRepository.findByUserId(userId);
+        Department department = restTemplate.getForObject("http://localhost:9001/departments/"+user.getDepartmentId(), Department.class);
+        vo.setUser(user);
+        vo.setDepartment(department);
+        return vo;
+    }
+}
---
+++ User-Service/src/main/java/com/akash/userservice/ValueObject/ResponseTemplateVO.java
@@ -0,0 +1,14 @@
+package com.akash.userservice.ValueObject;
+
+import com.akash.userservice.Entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseTemplateVO {
+    private User user;
+    private Department department;
+}
